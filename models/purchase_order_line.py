53fb73aab081519048b7569356f9e22ad9c3a540
dovahir <lara.trabajo@proton.me>
2026-08-06 17:29:32 -0600

    eliminado campo note en order.line

--- models/purchase_order_line.py
+++ models/purchase_order_line.py
@@ -2,11 +2,8 @@
 from odoo import api, fields, models
 
 
-# Temporal para pruebas local ELIMINAR SOLO CAMPO "note":
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
-
-    note = fields.Char(string='Nota', help='Add note for product')
 
     # Asignar las #Req de las líneas de compra a la recepción
     def _prepare_stock_move_vals(self, picking, price_unit, product_uom_qty, product_oum):
